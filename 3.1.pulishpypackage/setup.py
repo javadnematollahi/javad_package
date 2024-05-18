919757d0dcc5f2aef34d95ad040b8958f29ccf31
j_nematollahi <javad7189@yahoo.com>
2024-05-18 19:46:10 +0330

    add setup

---
+++ 3.1.pulishpypackage/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+def pre_install():
+    f = open("readme.md", "r")
+    text = f.read()
+    return text
+
+setup(
+    name= "javad_cow",
+    version= "1.0.0",
+    author="javad",
+    description="A test package for pydeploy",
+    long_description=pre_install(),
+    requires=[],
+)
